7214051612afa03cf2ab212f5759c1a40057e505
75py <dev.75py@gmail.com>
2017-11-24 16:47:30 +0900

    Update Travis settings

--- library/src/androidTest/java/com/nagopy/android/overlayviewmanager/OverlayViewManagerTest.java
+++ library/src/androidTest/java/com/nagopy/android/overlayviewmanager/OverlayViewManagerTest.java
@@ -5,14 +5,20 @@ import android.app.Application;
 import android.app.Instrumentation;
 import android.app.UiAutomation;
 import android.os.Build;
+import android.os.ParcelFileDescriptor;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
+import android.util.Log;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertFalse;
@@ -40,25 +46,34 @@ public class OverlayViewManagerTest {
         uiAutomation = InstrumentationRegistry.getInstrumentation().getUiAutomation();
     }
 
-    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.M)
+    // Cannot grant SYSTEM_ALERT_WINDOW permission by "pm grant pkg" command on Marshmallow.
+    // So skip the test.
+    // @SdkSuppress(minSdkVersion = Build.VERSION_CODES.M)
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.N)
     @Test
-    public void canDrawOverlays_API23_grant() throws Exception {
-        uiAutomation.executeShellCommand("pm grant "
-                + application.getPackageName()
-                + " "
-                + Manifest.permission.SYSTEM_ALERT_WINDOW);
-        Thread.sleep(2000);
+    public void canDrawOverlays_granted() throws Exception {
+        executeShellCommand(
+                "pm grant "
+                        + application.getPackageName()
+                        + " "
+                        + Manifest.permission.SYSTEM_ALERT_WINDOW
+                , 5000);
+
         assertTrue(overlayViewManager.canDrawOverlays());
     }
 
-    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.M)
+    // Cannot grant SYSTEM_ALERT_WINDOW permission by "pm grant pkg" command on Marshmallow.
+    // So skip the test.
+    // @SdkSuppress(minSdkVersion = Build.VERSION_CODES.M)
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.N)
     @Test
-    public void canDrawOverlays_API23_deny() throws Exception {
-        uiAutomation.executeShellCommand("pm revoke "
-                + application.getPackageName()
-                + " "
-                + Manifest.permission.SYSTEM_ALERT_WINDOW);
-        Thread.sleep(2000);
+    public void canDrawOverlays_denied() throws Exception {
+        executeShellCommand(
+                "pm revoke "
+                        + application.getPackageName()
+                        + " "
+                        + Manifest.permission.SYSTEM_ALERT_WINDOW
+                , 5000);
         assertFalse(overlayViewManager.canDrawOverlays());
     }
 
@@ -76,6 +91,27 @@ public class OverlayViewManagerTest {
     @Test
     public void getDisplayHeight() throws Exception {
         assertThat(overlayViewManager.getDisplayHeight(), is(uiDevice.getDisplayHeight()));
+    }
+
+    private void executeShellCommand(String cmd, long timeoutInMillis) throws Exception {
+        ParcelFileDescriptor pfDescriptor = uiAutomation.executeShellCommand(cmd);
+        long endTimeInMillis = timeoutInMillis > 0 ? System.currentTimeMillis() + timeoutInMillis : 0;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(new ParcelFileDescriptor.AutoCloseInputStream(pfDescriptor)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.i("canDrawOverlays_API23_deny", line);
+                if (endTimeInMillis > System.currentTimeMillis()) {
+                    throw new TimeoutException();
+                }
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
     }
 
 }
