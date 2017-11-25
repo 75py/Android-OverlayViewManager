669ca10793d0ce61a7c2f60d99f6eb98a13e7ea5
75py <dev.75py@gmail.com>
2017-11-25 18:15:21 +0900

    Run instrument tests

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -5,9 +5,9 @@ set -ev
 echo "script: $TEST_TYPE"
 
 if [ "$TEST_TYPE" == "unit" ]; then
-    ./gradlew testDebug lintDebug jacocoTestDebugUnitTestReport --stacktrace
+    ./gradlew testDebug lintDebug jacocoTestDebugUnitTestReport --stacktrace --info
 elif [ "$TEST_TYPE" == "instrument" ]; then
-    ./gradlew connectedDebugAndroidTest --stacktrace
+    ./gradlew connectedDebugAndroidTest --stacktrace --info
 else
     exit 1
 fi
